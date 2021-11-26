09e32d06d5f919f43c569a154ff955c06e578af5
C.Milona <xr.milona@gmail.com>
2021-11-26 20:50:54 +0200

    change mongoose options

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+ });
 
 mongoose.connection.on(
   'error',
